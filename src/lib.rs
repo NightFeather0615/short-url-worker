a5ff4ab7af45e956ed1e3753110aac2fcf81b895
NightFeather <leolee50910@gmail.com>
2024-05-22 05:00:55 +0800

    impl cache control

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,9 @@ use worker::*;
 
 #[event(fetch)]
 async fn main(req: Request, env: Env, _ctx: Context) -> Result<Response> {
-  let kv: kv::KvStore = env.kv("ShortUrl")?;
+  if req.method() != Method::Get {
+    return Response::error("Method Not Allowed", 405);
+  }
 
   let target_path: String = req.path().trim_start_matches("/").to_string();
 
@@ -10,8 +12,34 @@ async fn main(req: Request, env: Env, _ctx: Context) -> Result<Response> {
     return Response::error("Bad URL Path", 400);
   }
 
-  return match kv.get(target_path.as_str()).text().await? {
-    Some(dest_url) => Response::redirect(Url::parse(dest_url.as_str())?),
-    None => Response::error("Destination Not Found", 404)
-  };
+  let cache: Cache = Cache::open("shortUrlKv:cache".to_string()).await;
+
+  if let Some(cache_result) = cache.get(CacheKey::Request(&req), true).await? {
+    return Ok(cache_result);
+  }
+
+  let kv: kv::KvStore = env.kv("ShortUrl")?;
+
+  let mut cache_headers: Headers = Headers::new();
+  cache_headers.append("Cache-Control", "public, max-age=86400")?;
+
+  if let Some(dest_url) = kv.get(&target_path).text().await? {
+    cache_headers.append("Location", &dest_url)?;
+
+    let mut res: Response = Response::builder()
+      .with_headers(cache_headers)
+      .with_status(302)
+      .ok("Found")?;
+    cache.put(CacheKey::Request(&req), res.cloned()?).await?;
+    
+    return Ok(res);
+  } else {
+    let mut res: Response = Response::builder()
+      .with_headers(cache_headers)
+      .with_status(404)
+      .ok("Destination Not Found")?;
+    cache.put(CacheKey::Request(&req), res.cloned()?).await?;
+
+    return Ok(res);
+  }
 }
